f67da466c62851c031d6b6bd4294ec42293f2510
Bindu <bindukrishnamurthyid@gmail.com>
2026-05-21 20:03:12 +0530

    message added

---
+++ src/main/java/com/example/CICD20231CDV0041/Calculator.java
@@ -0,0 +1,10 @@
+package com.example.CICD20231CDV0041;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/example/CICD20231CDV0041/AppTest.java
@@ -0,0 +1,13 @@
+package com.example.CICD20231CDV0041;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
